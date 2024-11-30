9e7c3661a48fdff20ec8737c8a85255b3f87ed7f
Caleb <calebharper77@gmail.com>
2024-11-29 21:43:01 -0600

    shareFile

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchURL(Uri url, LaunchMode? launchMode) async {
@@ -40,4 +43,25 @@ String toTitleCase(String input) {
       ? '${word[0].toUpperCase()}${word.substring(1).toLowerCase()}'
       : word)
     .join(' ');
+}
+
+Future<bool> shareFile(bool allowShareContent, String subject, File file, String content) async {
+  try {
+    await Share.shareXFiles([XFile(file.path)], text: subject);
+    return true;
+  } catch (e) {
+    if (allowShareContent) {
+      print("Unable to Share.shareXFiles: falling back on Share.share: $e");
+      try {
+        await Share.share(content, subject: subject);
+        return true;
+      } catch (e2) {
+        print("Unable to Share.share: $e2");
+        return false;
+      }
+    } else {
+      print("Unable to Share.share: action not allowed");
+      return false;
+    }
+  }
 }
