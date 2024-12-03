ffa356d0b33e7159f96f0b63dcad2eab64544017
Caleb <calebharper77@gmail.com>
2024-12-03 17:56:07 -0600

    customTheme parameters not required and have defaults

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData customTheme({
-  required bool darkMode,
-  required Color seedColor,
-  required TextTheme textStyle,
+  bool? darkMode,
+  Color? seedColor,
+  TextTheme? textStyle,
 }) {
+  TextTheme textTheme = textStyle ?? GoogleFonts.poppinsTextTheme();
+  bool useDarkMode = darkMode ?? false;
   return ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: seedColor,
-      brightness: darkMode ? Brightness.dark : Brightness.light,
+      seedColor: seedColor ?? Colors.amber,
+      brightness: useDarkMode ? Brightness.dark : Brightness.light,
     ),
     useMaterial3: true,
-    textTheme: textStyle.apply(
-      bodyColor: darkMode ? Colors.white : Colors.black,
-      displayColor: darkMode ? Colors.white : Colors.black,
+    textTheme: textTheme.apply(
+      bodyColor: useDarkMode ? Colors.white : Colors.black,
+      displayColor: useDarkMode ? Colors.white : Colors.black,
     ),
   );
 }
