16f72a6e9ad37fa61a5e46e6036e3f9caba9bd39
Caleb <calebharper77@gmail.com>
2024-12-03 18:15:13 -0600

    customTheme iconSize

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 ThemeData customTheme({
-  bool? darkMode,
-  Color? seedColor,
+  bool darkMode = false,
+  Color seedColor = Colors.red,
   TextTheme? textStyle,
+  double iconSize = 30,
 }) {
   TextTheme textTheme = textStyle ?? GoogleFonts.poppinsTextTheme();
-  bool useDarkMode = darkMode ?? false;
   return ThemeData(
     colorScheme: ColorScheme.fromSeed(
-      seedColor: seedColor ?? Colors.amber,
-      brightness: useDarkMode ? Brightness.dark : Brightness.light,
+      seedColor: seedColor,
+      brightness: darkMode ? Brightness.dark : Brightness.light,
     ),
     useMaterial3: true,
     textTheme: textTheme.apply(
-      bodyColor: useDarkMode ? Colors.white : Colors.black,
-      displayColor: useDarkMode ? Colors.white : Colors.black,
+      bodyColor: darkMode ? Colors.white : Colors.black,
+      displayColor: darkMode ? Colors.white : Colors.black,
     ),
+    iconTheme: IconThemeData(size: iconSize),
   );
 }
