1696b957178b2217d3121c5dd9d8c49675376c67
Caleb <calebharper77@gmail.com>
2025-01-06 21:54:14 -0600

    undo last and remove awaits

--- lib/online.dart
+++ lib/online.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
@@ -128,17 +127,17 @@ Future<bool> serverlaunch(context) async {
       var message = general["message"];
       if (config["message"]) {
         print("server.launch status: message");
-        await showAlertDialogue(context, "Server Message", message["message"], false, {"show": true});
+        showAlertDialogue(context, "Server Message", message["message"], false, {"show": true});
         status = true;
       }
       if (config["warning"]) {
         print("server.launch status: warning");
-        await showAlertDialogue(context, "Server Message: Warning", message["warning"], false, {"show": true});
+        showAlertDialogue(context, "Server Message: Warning", message["warning"], false, {"show": true});
         status = true;
       }
       if (config["disable"]) {
         print("server.launch status: disable");
-        showConstantDialogue(context, "Server Disabled", message["disable"]);
+        showAlertDialogue(context, "Server Disabled", message["disable"], false, {"show": true});
         status = false;
       }
       return status;
