b2da4da4ebaa027abb9bee0ebf08803070dffe5a
Caleb <calebharper77@gmail.com>
2025-01-30 19:42:33 -0600

    CrashPage

--- lib/error.dart
+++ lib/error.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:localpkg/functions.dart';
 import 'package:localpkg/logger.dart' as logger;
 
 class ManualError extends Error {
@@ -22,5 +24,43 @@ class ManualError extends Error {
 
   void invoke({String? code, bool? trace}) {
     throw Exception(toString());
+  }
+}
+
+class CrashPage extends StatefulWidget {
+  const CrashPage({super.key});
+
+  @override
+  State<CrashPage> createState() => _CrashPageState();
+}
+
+class _CrashPageState extends State<CrashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.warning_rounded,
+                color: Colors.amber,
+                size: 72,
+              ),
+              Text("Whoa!", style: TextStyle(fontSize: 32, color: Colors.redAccent)),
+              Text("This application is meant to be run on desktop PCs or Macs. If this is a mistake, please contact support."),
+              TextButton(
+                child: Text("Support"),
+                onPressed: () {
+                  support(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
